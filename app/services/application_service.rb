20ed710e1e3823b19ed083cb4598b8dc6422ead5
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2024-07-31 19:43:04 -0300

    Fix rubocop offenses

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationService
   def self.call(...)
     new(...).call
   end
 
   def call
-    raise NotImplementedError, "missing implementation of call method"
+    raise NotImplementedError, 'missing implementation of call method'
   end
 end
--- app/services/data_builder/tarkov/items.rb
+++ app/services/data_builder/tarkov/items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module DataBuilder
   module Tarkov
     class Items < ApplicationService
